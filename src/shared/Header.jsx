9202c1f438d86de252a23c8592228633115d7f98
computer-note <planomenes@gmail.com>
2024-06-13 21:08:32 +0900

    add : 헤더 컴포넌트에 각 페이지로 이동할 수 있는 링크 추가

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 function Header() {
-  return <nav>헤더헤더</nav>;
+  return (
+    <StNav>
+      {/* 인증상태 false이면 표시할 UI */}
+      <Link to='/login'>로그인</Link>
+      <Link to='/signup'>회원가입</Link>
+
+      {/* 인증상태 true이면 표시할 UI */}
+      <Link to='/mypage'>나의페이지</Link>
+      <button>로그아웃</button>
+    </StNav>
+  );
 }
+
+//질문: StNav 정의 이전에 사용해도 되는 이유
+const StNav = styled.nav`
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  margin-right: 2rem;
+  gap: 2rem;
+  height: 6rem;
+  font-size: 1.3rem;
+  background-color: aliceblue;
+`;
 
 export default Header;
