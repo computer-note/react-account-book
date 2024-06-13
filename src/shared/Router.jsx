f4f14d3e6f6b4fe042da090df2a592eb633daca3
computer-note <planomenes@gmail.com>
2024-06-14 00:32:50 +0900

    update : 로그인 하지 않으면 로그인 페이지로 리다이렉트
    Todo: 강의에서 사용하는 Public/Private Router 방식 공부하기

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,22 +1,58 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import DetailPage from '../pages/DetailPage';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useNavigate,
+} from 'react-router-dom';
+
 import Header from './Header';
-import MyPage from './../pages/MyPage';
+import HomePage from '../pages/HomePage';
 import LoginPage from './../pages/LoginPage';
-import SignupPage from '../pages/SignupPage';
+import SignupPage from './../pages/SignupPage';
+import MyPage from './../pages/MyPage';
+import DetailPage from '../pages/DetailPage';
+
+import { useContext, useEffect } from 'react';
+
+import { AuthContext } from '../context/AuthContextProvider';
+
+function PublicRouter() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //질문 아래 alert 사용시 오래걸리는 이유
+    //alert('로그인을 해주세요');
+    navigate('/login');
+  }, []);
+
+  return <></>;
+}
 
 function Router() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/mypage' element={<MyPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/detail/:id' element={<DetailPage />} />
-      </Routes>
+      {isAuthenticated ? (
+        //로그인시 접근 가능한 페이지
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/mypage' element={<MyPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/detail/:id' element={<DetailPage />} />
+        </Routes>
+      ) : (
+        //비로그인시 리다이렉트되는 페이지 => PublicRouter
+        <Routes>
+          <Route path='/' element={<PublicRouter />} />
+          <Route path='/mypage' element={<PublicRouter />} />
+          <Route path='/detail/:id' element={<PublicRouter />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
