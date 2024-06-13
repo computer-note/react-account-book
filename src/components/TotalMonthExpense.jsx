de5109d342560d347a3af21c3473dd5531122a7d
computer-note <planomenes@gmail.com>
2024-06-14 02:54:37 +0900

    update : 월별 총 지출액 계산에 필요한 데이터를 DB에서 로드하도록 변경

--- src/components/TotalMonthExpense.jsx
+++ src/components/TotalMonthExpense.jsx
@@ -1,28 +1,34 @@
-import { useContext } from 'react';
-import { useSelector } from 'react-redux';
-
-import { AppContext } from '../context/AppContextProvider';
 import styled from 'styled-components';
 
-const StDiv = styled.div`
-  background-color: mediumaquamarine;
-  height: 100px;
-  width: 500px;
-  border-radius: 20px;
-  font-size: xx-large;
-  display: flex;
-  align-items: center;
-`;
+import { useContext } from 'react';
+import { AppContext } from '../context/AppContextProvider';
 
+import { useQuery } from '@tanstack/react-query';
+import { readAllAccountList } from '../axios/accountApi';
+
+//Todo: 유틸리티 모듈로 빼기
 function extractMonthFromDate(date) {
-  //YYYY-MM-DD
+  //YYYY-MM-DD => MM
   return date.slice(5, 7);
 }
 
 function TotalMonthExpense() {
-  const entryList = useSelector(state => state.entryListSlice);
   const { selectedMonth } = useContext(AppContext);
 
+  //Todo: 가져올 때 월별로 filter해서 가져오는 API있는지 알아보기
+  const accountListQuery = useQuery({
+    queryFn: readAllAccountList,
+    queryKey: ['accountList'],
+  });
+
+  //Todo: DB에서는 가계부항목을 Account라고 부르는데 클라이언트에서는 Entry라고 부르는 문제 해결하기
+  const { isPending, isError, data: entryList } = accountListQuery;
+
+  if (isPending || isError) {
+    return <p>로딩 중 혹은 에러 발생</p>;
+  }
+
+  //Todo: 적절한 Memoization 적용하기
   const totalMonthExpense = entryList
     .filter(entry => {
       const entryMonth = +extractMonthFromDate(entry.date);
@@ -38,4 +44,15 @@ function TotalMonthExpense() {
     </StDiv>
   );
 }
+
+const StDiv = styled.div`
+  background-color: mediumaquamarine;
+  height: 100px;
+  width: 500px;
+  border-radius: 20px;
+  font-size: xx-large;
+  display: flex;
+  align-items: center;
+`;
+
 export default TotalMonthExpense;
