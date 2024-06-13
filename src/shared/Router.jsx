6487c9cb8ef7d6dd301317c9b5be4b40e853df6e
computer-note <planomenes@gmail.com>
2024-06-13 21:06:33 +0900

    add : 회원인증 기능에 필요한 페이지 생성 및 라우터에 연결 1. 로그인페이지, 회원가입페이지 추가 2. 나의 회원정보를 볼 수 있는 마이페이지 추가 3. 라우터에서 각 페이지에 경로 할당

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import DetailPage from '../pages/DetailPage';
 import Header from './Header';
+import MyPage from './../pages/MyPage';
+import LoginPage from './../pages/LoginPage';
+import SignupPage from '../pages/SignupPage';
 
 function Router() {
   return (
@@ -9,7 +12,10 @@ function Router() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/Detail/:id' element={<DetailPage />} />
+        <Route path='/mypage' element={<MyPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/detail/:id' element={<DetailPage />} />
       </Routes>
     </BrowserRouter>
   );
