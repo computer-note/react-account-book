4ecdd8e4d7359f354ccbeec29c7dbb32bc0fda33
computer-note <planomenes@gmail.com>
2024-06-13 20:59:51 +0900

    update : Header 컴포넌트를 Router 컴포넌트 내부로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import GlobalStyle from './shared/GlobalStyle';
-import Header from './shared/Header';
 import Router from './shared/Router';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
       <Router />
     </>
   );
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import DetailPage from '../pages/DetailPage';
+import Header from './Header';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/Detail/:id' element={<DetailPage />} />
