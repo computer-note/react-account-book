31eab6995362fc219ed5127d345e2309b0bd8f2c
computer-note <planomenes@gmail.com>
2024-06-13 23:21:30 +0900

    update: (1) 공통헤더 조건부 렌더링 (2) 로그아웃 버튼 로직
    (1) 앱의 인증상태에 따라 공통헤더의 UI를 조건부 렌더링합니다.
    (2) 공통헤더의 로그아웃 버튼을 클릭시 클라이언트에서 앱상태를 비인증상태로 변경합니다.
      Todo. logout API 구현 및 연동하기

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,18 +1,52 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContextProvider';
 import styled from 'styled-components';
 
 function Header() {
+  const { isAuthenticated, setAppStateLogOut } =
+    useContext(AuthContext);
+  const navigate = useNavigate();
+
+  async function handleLogoutButtonClick() {
+    //Todo. logout API 호출 후 응답 대기
+    setAppStateLogOut();
+
+    alert('로그아웃');
+    navigate('/');
+  }
+
   return (
     <StNav>
-      {/* 인증상태 false이면 표시할 UI */}
-      <Link to='/login'>로그인</Link>
-      <Link to='/signup'>회원가입</Link>
-
-      {/* 인증상태 true이면 표시할 UI */}
-      <Link to='/mypage'>나의페이지</Link>
-      <button>로그아웃</button>
+      <p>현재인증상태:{isAuthenticated.toString()}</p>
+      {isAuthenticated ? (
+        /* 인증상태 true이면 표시할 UI */
+        <>
+          <Link to='/mypage'>나의페이지</Link>
+          <button onClick={handleLogoutButtonClick}>로그아웃</button>
+        </>
+      ) : (
+        /* 인증상태 false이면 표시할 UI */
+        <>
+          <Link to='/login'>로그인</Link>
+          <Link to='/signup'>회원가입</Link>
+        </>
+      )}
     </StNav>
   );
+
+  // 이후 로그인 되었는데 로그인 페이지로 가거나 하는 경우를 테스트하기 편하게 하기 위한 세팅
+  // return (
+  //   <StNav>
+  //     <p>현재인증상태:{isAuthenticated.toString()}</p>
+  //     {/* 인증상태 true이면 표시할 UI */}
+  //     <Link to='/mypage'>나의페이지</Link>
+  //     <button>로그아웃</button>
+  //     {/* 인증상태 false이면 표시할 UI */}
+  //     <Link to='/login'>로그인</Link>
+  //     <Link to='/signup'>회원가입</Link>
+  //   </StNav>
+  // );
 }
 
 //질문: StNav 정의 이전에 사용해도 되는 이유
