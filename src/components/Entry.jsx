50bb175f6837d54165966401a5a7860d8179da76
computer-note <planomenes@gmail.com>
2024-06-14 02:08:49 +0900

    update | 메인페이지: 가계부 항목 표시할 때 닉네임도 추가 표시

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,39 +1,42 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StDiv = styled.div`
-   border: 1px black solid;
-   border-radius: 10px;
-   height: 200px;
+function Entry({ entryData }) {
+  const { date, title, expense, description, nickname, id } =
+    entryData;
 
-   &:hover {
-      background-color: lightskyblue;
-   }
+  const navigate = useNavigate();
+
+  function handleEntryClick() {
+    navigate(`/detail/${id}`);
+  }
+
+  return (
+    <StEntryDiv onClick={handleEntryClick}>
+      <p> 작성자: {nickname}</p>
+      <p> 날짜: {date} </p>
+      <p> 제목: {title} </p>
+      <p> 지출: {expense} 원 </p>
+      <StP> 내용: {description} </StP>
+    </StEntryDiv>
+  );
+}
+
+const StEntryDiv = styled.div`
+  border: 1px black solid;
+  border-radius: 10px;
+  height: 200px;
+  padding: 10px;
+
+  &:hover {
+    background-color: lightskyblue;
+  }
 `;
 
 const StP = styled.p`
-   white-space: nowrap;
-   overflow: hidden;
-   text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
-function Entry(props) {
-   const { date, title, expense, description, id } = props.entryData;
-
-   const navigate = useNavigate();
-
-   function handleEntryClick() {
-      const url = `/Detail/${id}`;
-      navigate(url);
-   }
-
-   return (
-      <StDiv onClick={handleEntryClick}>
-         <p> 날짜: {date} </p>
-         <p> 제목: {title} </p>
-         <p> 지출: {expense} 원 </p>
-         <StP> 내용: {description} </StP>
-      </StDiv>
-   );
-}
 export default Entry;
--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -1,10 +1,42 @@
-import { useContext } from 'react';
 import Entry from './Entry';
 import { styled } from 'styled-components';
+
+import { useContext } from 'react';
+
 import { AppContext } from '../context/AppContextProvider';
-import { useSelector } from 'react-redux';
+
 import { useQuery } from '@tanstack/react-query';
 import { readAllAccountList } from '../axios/accountApi';
+
+function EntryList() {
+  const { selectedMonth } = useContext(AppContext);
+
+  const accountListQuery = useQuery({
+    queryFn: readAllAccountList,
+    queryKey: ['accountList'],
+  });
+
+  const { isPending, isError, data: entryList } = accountListQuery;
+
+  if (isPending || isError) {
+    return <p>로딩 중 혹은 에러 발생</p>;
+  }
+
+  //Todo. 선택된 달에 맞춰서 필터링하는 로직 위로 빼기 + useMemo
+
+  return (
+    <StSection>
+      <StP> {selectedMonth} 월의 가계부</StP>
+      {entryList.map(entryData => {
+        const entryMonth = Number(entryData.date.slice(5, 7));
+
+        if (entryMonth === selectedMonth) {
+          return <Entry key={entryData.id} entryData={entryData} />;
+        }
+      })}
+    </StSection>
+  );
+}
 
 const StSection = styled.section`
   border: 1px black solid;
@@ -20,34 +52,4 @@ const StP = styled.p`
   background-color: lightcyan;
 `;
 
-function EntryList() {
-  //const entryList = useSelector(state => state.entryListSlice);
-  const { selectedMonth } = useContext(AppContext);
-
-  const accountListQuery = useQuery({
-    queryFn: readAllAccountList,
-    queryKey: ['accountList'],
-  });
-  console.log('accountListQuery ↓');
-  console.dir(accountListQuery);
-
-  const { isPending, isError, data: entryList } = accountListQuery;
-
-  if (isPending || isError) {
-    return <p>로딩 중 혹은 에러 발생</p>;
-  }
-
-  return (
-    <StSection>
-      <StP> {selectedMonth} 월의 가계부</StP>
-      {entryList.map(entryData => {
-        const entryMonth = Number(entryData.date.slice(5, 7));
-
-        if (entryMonth === selectedMonth) {
-          return <Entry key={entryData.id} entryData={entryData} />;
-        }
-      })}
-    </StSection>
-  );
-}
 export default EntryList;
