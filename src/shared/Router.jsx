c1cae129d056c7158450c87e65a68285ce321a37
computer-note <planomenes@gmail.com>
2024-06-25 21:18:07 +0900

    update (Router.jsx) : PublicRoute / PrivateRoute 패턴 적용

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,58 +1,96 @@
 import {
   BrowserRouter,
+  Navigate,
   Route,
   Routes,
   useNavigate,
 } from 'react-router-dom';
 
 import Header from './Header';
+
 import HomePage from '../pages/HomePage';
+import DetailPage from '../pages/DetailPage';
 import LoginPage from './../pages/LoginPage';
 import SignupPage from './../pages/SignupPage';
 import MyPage from './../pages/MyPage';
-import DetailPage from '../pages/DetailPage';
 
 import { useContext, useEffect } from 'react';
 
 import { AuthContext } from '../context/AuthContextProvider';
 
-function PublicRouter() {
-  const navigate = useNavigate();
+// function PublicRouter() {
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    //질문 아래 alert 사용시 오래걸리는 이유
-    //alert('로그인을 해주세요');
-    navigate('/login');
-  }, []);
+//   useEffect(() => {
+//     //질문 아래 alert 사용시 오래걸리는 이유
+//     //alert('로그인을 해주세요');
+//     navigate('/login');
+//   }, []);
 
-  return <></>;
+//   return <></>;
+// }
+
+//로그인을 한 유저가 PublicRoute에 접근할 경우 마이페이지로 리다이렉트
+function PublicRoute({ element: PageComponent, ...rest }) {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  if (isAuthenticated) {
+    alert('로그인이 되어있습니다. 마이페이지로 이동합니다.');
+  }
+
+  return !isAuthenticated ? (
+    <PageComponent {...rest} />
+  ) : (
+    <Navigate to='/mypage' />
+  );
+}
+
+//로그인 하지 않은 유저가 PrivateRoute에 접근할 경우 로그인페이지로 리다이렉트
+function PrivateRoute({ element: PageComponent, ...rest }) {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  if (isAuthenticated) {
+    alert('로그인이 되어있지 않습니다. 로그인페이지로 이동합니다.');
+  }
+
+  return isAuthenticated ? (
+    <PageComponent {...rest} />
+  ) : (
+    <Navigate to='/login' />
+  );
 }
 
 function Router() {
-  const { isAuthenticated } = useContext(AuthContext);
-
   return (
     <BrowserRouter>
       <Header />
-      {isAuthenticated ? (
-        //로그인시 접근 가능한 페이지
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/mypage' element={<MyPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignupPage />} />
-          <Route path='/detail/:id' element={<DetailPage />} />
-        </Routes>
-      ) : (
-        //비로그인시 리다이렉트되는 페이지 => PublicRouter
-        <Routes>
-          <Route path='/' element={<PublicRouter />} />
-          <Route path='/mypage' element={<PublicRouter />} />
-          <Route path='/detail/:id' element={<PublicRouter />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignupPage />} />
-        </Routes>
-      )}
+      <Routes>
+        {/* <Route
+          path='/login'
+          element={<PublicRoute element={<LoginPage />} />}
+        /> */}
+        <Route
+          path='/'
+          element={<PrivateRoute element={HomePage} />}
+        />
+        <Route
+          path='/detail'
+          element={<PrivateRoute element={DetailPage} />}
+        />
+        <Route
+          path='/mypage'
+          element={<PrivateRoute element={MyPage} />}
+        />
+
+        <Route
+          path='/login'
+          element={<PublicRoute element={LoginPage} />}
+        />
+        <Route
+          path='/signup'
+          element={<PublicRoute element={SignupPage} />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
